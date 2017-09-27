6dc020c2120bbf8c7700bac4570adbda714b7f58
apgyapgy <2648226617@qq.com>
2017-09-27 17:33:15 +0800

    首页与我的页面好了

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,47 @@ const app = getApp()
 Page({
   data: {
     autoplay:false,
-    bannerImgs:['../../image/banner.png']
+    bannerImgs:['../../image/banner.png'],
+    backTopIconShowFlag:false,
+    scrollTop:0,
+    tabbarArray:[
+      {
+        id:1,
+        cls:'home',
+        url:'/pages/index/index',
+        src:'../../image/home.png',
+        active_src:'../../image/home_active.png',
+        text:'首页',
+        active:true
+      },
+      {
+        id: 2,
+        cls: 'car',
+        url: '/pages/shoppingcat/shoppingcart',
+        src: '../../image/car.png',
+        active_src:'../../image/car_active.png',
+        text: '购物车',
+        shopCartNum: 0,
+        active: false
+      },
+      {
+        id: 3,
+        cls: 'order',
+        url: '/pages/order/order',
+        src: '../../image/order.png',
+        active_src: '../../image/order_active.png',
+        text: '订单',
+        active: false
+      }, {
+        id: 4,
+        cls: 'my',
+        url: '/pages/my/my',
+        src: '../../image/my.png',
+        active_src: '../../image/my_active.png',
+        text: '我的',
+        active: false
+      }
+    ]
   }, 
   onLoad: function () {
     
@@ -29,8 +69,26 @@ Page({
   },
   //返回顶部
   goTop:function(){
-    wx.pageScrollTo({
+    /*wx.pageScrollTo({
       scrollTop: 0
+    });*/
+    this.setData({
+      scrollTop:0
     });
+  },
+  checkBackTop:function(e){//检测并判断是否显示返回顶部按钮 
+    if (e.detail.scrollTop > 500) {
+      if (this.data.backTopIconShowFlag == false){
+        this.setData({
+          backTopIconShowFlag: true
+        });
+      }
+    } else {
+      if (this.data.backTopIconShowFlag == true) {
+        this.setData({
+          backTopIconShowFlag: false
+        });
+      }
+    }
   }
 })
